cd4d8cfbe2996dcd22f21130daa302d805ee6423
shanjee <shanjeeva6602@gmail.com>
2015-08-02 11:50:43 +0530

    new booking added

--- SMLApplication-master/SMLApplication.Web/App_Start/BundleConfig.cs
+++ SMLApplication-master/SMLApplication.Web/App_Start/BundleConfig.cs
@@ -37,7 +37,8 @@ namespace SMLApplication.Web
                         "~/Content/themes/base/jquery.ui.tabs.css",
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                        "~/Content/themes/base/jquery.ui.theme.css",
+                        "~/Content/themes/base/jquery-ui.css"));
         }
     }
 }
