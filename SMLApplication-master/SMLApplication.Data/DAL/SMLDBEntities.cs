eaa08fbdcecb0e09d1870931f40616f57c5e133f
ashenfern <ashenfern@gmail.com>
2015-08-04 06:26:01 +0530

    Checking changes in the constructor of the DBEntities

--- SMLApplication-master/SMLApplication.Data/DAL/SMLDBEntities.cs
+++ SMLApplication-master/SMLApplication.Data/DAL/SMLDBEntities.cs
@@ -15,7 +15,12 @@ namespace SMLApplication.Data.DAL
         public DbSet<Appointment> Appointments { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Patient> Patients { get; set; }
-
         public DbSet<Booking> Bookings { get; set; }
+
+        public SMLDBEntities()
+            : base("name=SMLDBEntities")
+        {
+            base.Configuration.ProxyCreationEnabled = false;
+        }
     }
 }
